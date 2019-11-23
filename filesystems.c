4256988792e42db020a70d884ddb073a05cf2b97
Imtiaz <imtiazkhaled07@gmail.com>
2019-11-23 17:11:36 -0600

    shell implemented

--- filesystems.c
+++ filesystems.c
@@ -1,13 +1,32 @@
-#include <stdint.h>
-#include <sys/stat.h>
-#include <stdlib.h>
+/*
+  Imtiaz Mujtaba Khaled
+  1001551928
+
+  Sam Thomas
+
+*/
+
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
 
 
 #define BLOCK_SIZE 8192
 #define NUM_BLOCKS 4226
 #define NUM_FILES 128
+#define MAX_COMMAND_SIZE 255     
+#define MAX_NUM_ARGUMENTS 10
+#define HISTORY_NUM 15      
+#define SHELL_NAME "msh>"       
+#define WHITESPACE " \t\n"
 
 // File attributes
 #define NOTHING 0
@@ -35,7 +54,6 @@ struct Directory_Entry * dir;
 struct Inode *inodeList;
 uint8_t *freeBlockList;
 uint8_t *freeInodeList;
-
 
 void intializeBlockList() {
   int i;
@@ -135,7 +153,6 @@ int getFileIndex(char* filename) {
 
 // initializes a new file system
 void createfs(char *filename) {
-  int i;
   memset(blocks, 0,NUM_BLOCKS*BLOCK_SIZE);
   FILE *fd=fopen(filename,"w");
   intializeDirectory();
@@ -148,17 +165,27 @@ void createfs(char *filename) {
 
 // opens a file image and writes its contents into blocks
 void open(char * filename) {
-  FILE *fd = fopen(filename,"r");
-  fread(blocks,BLOCK_SIZE,NUM_BLOCKS,fd);
-  fclose(fd);
+  struct stat buffer;
+  if(stat(filename, &buffer) != 0) {
+    printf("open error: File not found\n");
+  } else {
+    FILE *fd = fopen(filename,"r");
+    fread(blocks,BLOCK_SIZE,NUM_BLOCKS,fd);
+    fclose(fd);
+  }
 }
 
 // write the block data into a file
-void close(char * filename) {
-  FILE *fd = fopen(filename,"w");
-  fwrite(blocks,BLOCK_SIZE,NUM_BLOCKS,fd);
-  fclose(fd);
-  memset(blocks, 0,NUM_BLOCKS*BLOCK_SIZE);
+void closeFile(char * filename) {
+  struct stat buffer;
+  if(stat(filename, &buffer) != 0) {
+    printf("close error: File not found\n");
+  } else {
+    FILE *fd = fopen(filename,"w");
+    fwrite(blocks,BLOCK_SIZE,NUM_BLOCKS,fd);
+    fclose(fd);
+    memset(blocks, 0,NUM_BLOCKS*BLOCK_SIZE);
+  }
 }
 
 // gets size that is remaining in the disk image
@@ -223,13 +250,32 @@ void put(char * filename) {
 
 // lists all the files in the file system
 void list() {
-  int i;
+  int i, flag=1;
   for(i = 0; i<NUM_FILES; i++){
     if(dir[i].valid == 1 && (inodeList[dir[i].inode].attributes != HIDDEN && inodeList[dir[i].inode].attributes != BOTH)){
       // checks to see if the file in the file system
       // is either hidden or both hidden and read-only
+      flag=0;
       printf("%6d\t%s\n",inodeList[dir[i].inode].size,dir[i].name);
     }
+  }
+  if(flag) {
+      printf("list: No files found.\n");
+  }
+}
+
+void listWithHidden() {
+  int i, flag=1;
+  for(i = 0; i<NUM_FILES; i++){
+    if(dir[i].valid == 1){
+      // checks to see if the file in the file system
+      // is either hidden or both hidden and read-only
+      flag=0;
+      printf("%6d\t%s\n",inodeList[dir[i].inode].size,dir[i].name);
+    }
+  }
+  if(flag) {
+      printf("list: No files found.\n");
   }
 }
 
@@ -286,48 +332,143 @@ void del(char* filename) {
   }
 }
 
+// This function handels all the shell operations 
+void shell_operations(char * cmd_str){
+  
+  int i;
+  // Ignores user input if it is a blank line
+  if(strcmp(cmd_str, "\n") == 0)  return;
+  
+  /* Parse input */
+  char *token[MAX_NUM_ARGUMENTS];
+  int   token_count = 0;         
+                                                         
+  // Pointer to point to the token
+  // parsed by strsep
+  char *arg_ptr;                                         
+                                                         
+  char *working_str  = strdup( cmd_str );
+
+  // we are going to move the working_str pointer so
+  // keep track of its original value so we can deallocate
+  // the correct amount at the end
+  char *working_root = working_str;
+  
+  // Stores the pid for the current process
+  pid_t curr_pid;
+  char *curr_command = strdup(working_root);
+  curr_command = strtok(curr_command, "\n");  // Removes trailing new line from input
+  
+  // Tokenize the input stringswith whitespace used as the delimiter
+  while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
+            (token_count<MAX_NUM_ARGUMENTS)){
+    token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
+    if( strlen( token[token_count] ) == 0 ){
+
+      token[token_count] = NULL;
+    }
+      token_count++;
+
+  }
+
+  if(strcmp(token[0],"exit") == 0 || strcmp(token[0],"quit") == 0){
+  // Exit when user input is exit or quit        
+    exit(0);
+  } else if(strcmp(token[0],"createfs") == 0) {
+    // creates a new file system with the user input
+    if(token_count == 2) {
+      printf("createfs: File not found\n");
+    } else {
+      createfs(token[1]);
+    }
+  } else if(strcmp(token[0],"open") == 0) {
+    // opens a previously made file system with the user input
+    if(token_count == 2) {
+      printf("open: File not found\n");
+    } else {
+      open(token[1]);
+    }
+  } else if(strcmp(token[0],"close") == 0) {
+    // closes a previously made file system with the user input
+    if(token_count == 2) {
+      printf("close: File not found\n");
+    } else {
+      closeFile(token[1]);
+    }
+  } else if(strcmp(token[0],"df") == 0) {
+    // prints the amount of free space in bytes remaining
+    printf("%ld bytes free.\n",df());
+  } else if(strcmp(token[0],"list") == 0) {
+    // lists all the files in the file system that are not hidden
+    if(token_count == 2) {
+        list();
+    } else {
+      if(strcmp(token[1],"-h") == 0) {
+        // if -h flag is given, then hidden files are also printed
+        listWithHidden();
+      } else {
+        list();
+      }
+    }
+  } else if(strcmp(token[0],"del") == 0) {
+    // lists all the files in the file system that are not hidden
+    if(token_count == 2) {
+      printf("del error: File not found\n");
+    } else {
+      del(token[1]);
+    }
+  } else if(strcmp(token[0],"attrib") == 0) {
+    // lists all the files in the file system that are not hidden
+    if(token_count < 4) {
+      printf("attrib error: Invalid command\n");
+    } else {
+      attrib(token[1],token[2]);
+    }
+  } else if(strcmp(token[0],"put") == 0) { 
+    // puts a file from the current directory into the file system
+    if(token_count == 2) {
+      printf("put: File not found\n");
+    } else {
+      put(token[1]);
+    }
+  }
+    
+}
+
+
+
 int main() {
   dir=(struct Directory_Entry*)&blocks[0];
   inodeList =(struct Inode*)&blocks[6];
   freeInodeList=(uint8_t*)&blocks[4];
   freeBlockList=(uint8_t*)&blocks[5];
+  
+  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+  
+  while( 1 ){      
+    // Print out the msh prompt
+    printf("%s ", SHELL_NAME);
 
-  // create fs 
-  createfs("disk.image");
-  
-  // opens fs
-  open("disk.image");
-  
-  // free space in fs
-  printf("current free space is %ld\n", df());
-  
-  // put into fs
-  put("test.txt");
-  
-  // change file attributes
-  attrib("+h","hi.there");
-  attrib("+r","hi.there");
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
+    shell_operations(cmd_str);
+  }
 
-  // list files in fs
-  list();
+  free(cmd_str);
   
-  // free space in fs again
-  printf("current free space is %ld\n", df());
-
-  // change file attributes
-  attrib("+h","hi.there");
-
-  // deletes file from the filesystem
-  del("hi.there");
+  // // put into fs
+  // put("test.txt");
   
-  // list files in fs
-  list();
+  // // change file attributes
+  // attrib("+h","hi.there");
+  // attrib("+r","hi.there");
   
-  // free space in fs again
-  printf("current free space is %ld\n", df());
-
-  // close fs
-  close("disk.image");
+  // // change file attributes
+  // attrib("+h","hi.there");
   
   return 0;
 }
