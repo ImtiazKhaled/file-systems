a9fc2da746b1ab7375eb9877bf44ea6bad0a31b4
Sam <cachecake@gmail.com>
2019-11-23 19:39:18 -0600

    One file implementation put and get

--- filesystems.c
+++ filesystems.c
@@ -1,3 +1,5 @@
+// Use copy like this copyFile("secret","NotSoSecret");
+
 #include <stdint.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -8,6 +10,7 @@
 #define BLOCK_SIZE 8192
 #define NUM_BLOCKS 4226
 #define NUM_FILES 128
+
 
 uint8_t blocks[NUM_BLOCKS][BLOCK_SIZE];
 
@@ -71,8 +74,8 @@ int findFreeInode() {
 
   for(i=0;i<128;i++) {
     if(freeInodeList[i]==1) {
-      ret=1;
-      freeInodeList[i]=0;
+      ret=i;
+      //freeInodeList[i]=0;
       break;
     }
   }
@@ -84,9 +87,9 @@ int findFreeDirectory() {
   int i;
 
   for(i=0;i<128;i++) {
-    if(dir[i].valid==-1) {
-      ret=1;
-      dir[i].valid=0;
+    if(dir[i].valid==0) {
+      ret=i;
+      //dir[i].valid=0;
       break;
     }
   }
@@ -99,8 +102,8 @@ int findFreeBlock() {
 
   for(i=0;i<NUM_BLOCKS;i++) {
     if(freeBlockList[i]==1) {
-      ret=1;
-      freeBlockList[i]=0;
+      ret=i;
+      //freeBlockList[i]=0;
       break;
     }
   }
@@ -111,11 +114,11 @@ void createfs(char *filename) {
   int i;
   memset(blocks, 0,NUM_BLOCKS*BLOCK_SIZE);
   FILE *fd=fopen(filename,"w");
-  fwrite(blocks,BLOCK_SIZE,NUM_BLOCKS,fd);
   intializeDirectory();
   intializeInodeList();
   intializeBlockList();
   intializeInodes();
+  fwrite(blocks,BLOCK_SIZE,NUM_BLOCKS,fd);
   fclose(fd);
 
 }
@@ -133,22 +136,180 @@ void close(char * filename) {
   memset(blocks, 0,NUM_BLOCKS*BLOCK_SIZE);
 }
 
+
+int copyFile(char * source,int indexB)
+{
+  int    status;                  
+  struct stat buf;            
+  status =  stat( source, &buf ); 
+
+  if( status != -1 )
+  {
+ 
+    FILE *ifp = fopen ( source, "r" ); 
+    if(ifp==NULL)
+    {
+      return -1;
+    }
+    printf("Reading %d bytes from %s\n", (int) buf . st_size, source );
+ 
+    int copy_size   = buf . st_size;
+    int offset      = 0;               
+    int block_index = 0;
+ 
+    while( copy_size > BLOCK_SIZE )
+    {
+
+      int freeBlockIndex = findFreeBlock();
+      fseek( ifp, offset, SEEK_SET );
+ 
+      
+      int bytes  = fread(&freeBlockList[freeBlockIndex], BLOCK_SIZE, 1, ifp );
+      inodeList[indexB].blocks[block_index] = freeBlockIndex;
+      inodeList[indexB].size += (uint32_t) BLOCK_SIZE;
+      //freeBlockList[block_index] = 0;
+
+      if( bytes == 0 && !feof( ifp ) )
+      {
+        printf("An error occured reading from the input file.\n");
+        return -1;
+      }
+      clearerr( ifp );
+      copy_size -= BLOCK_SIZE;
+      offset    += BLOCK_SIZE;
+      block_index ++;
+    }
+
+    if(copy_size > 0)
+    {
+      int freeBlockIndex = findFreeBlock();
+      fseek(ifp, offset, SEEK_SET);
+      int bytes  = fread( freeBlockList, copy_size, 1, ifp );
+      inodeList[indexB].blocks[block_index] = freeBlockIndex;
+      inodeList[indexB].size += (uint32_t) copy_size;
+      if( bytes == 0 && !feof( ifp ) )
+      {
+        printf("An error occured reading from the input file.\n");
+        return -1;
+      }
+      clearerr( ifp );
+      copy_size -= BLOCK_SIZE;
+      offset    += BLOCK_SIZE;
+      block_index ++;
+    }
+    fclose( ifp );
+    return 0;
+   }
+   return -1;
+ }
+
+
+
+
+int writeFile(char * source,int indexB)
+{
+  
+    FILE *ofp;
+    ofp = fopen(source, "w");
+
+    int block_index = 0;
+    int copy_size   = inodeList[indexB].size;
+    int offset      = 0;
+
+    printf("Writing %d bytes to %s\n", copy_size , source );
+    while( copy_size > 0 )
+    { 
+
+      int num_bytes;
+
+      if( copy_size < BLOCK_SIZE )
+      {
+        num_bytes = copy_size;
+      }
+      else 
+      {
+        num_bytes = BLOCK_SIZE;
+      }
+
+      fwrite(&freeBlockList[inodeList[indexB].blocks[block_index]], num_bytes, 1, ofp ); 
+
+      copy_size -= BLOCK_SIZE;
+      offset    += BLOCK_SIZE;
+      block_index ++;
+
+      fseek( ofp, offset, SEEK_SET );
+    }
+    fclose( ofp );
+ }
+
+
+int getFileInodeIndex(char * fil)
+{
+  int i;
+  for(i=0;i<128;i++) {
+    if(dir[i].valid==1 && strcmp(fil,dir[i].name)==0)
+    {
+        return i;
+    } 
+  }
+  return -1;
+}
+
+
+void put(char * fileName)
+{
+  int freeDirIndex = findFreeDirectory();
+  int freeInode = findFreeInode();
+  //printf("\n\nYay! Found a free directory at index %d and free inode at %d going to set it as used now even though I ain't copying anything!\n",freeDirIndex,freeInode);
+  
+  dir[freeDirIndex].valid=1;
+  dir[freeDirIndex].inode = freeInode;
+  strcpy (dir[freeDirIndex].name, fileName);
+  freeInodeList[freeInode] = 0;
+  inodeList[freeInode].attributes = 0;
+  inodeList[freeInode].size  = 0; // This will change later depending on file size;
+  int copyCheck = copyFile(fileName,freeInode);
+  if(copyCheck==-1)
+  {
+    printf("\nSomething went wrong when trying to copy, make sure the file exsists!\n");
+  }
+}
+
+void get(char * fileNameToRead,char * fileToWrite)
+{
+  int getFileNode = getFileInodeIndex(fileNameToRead);
+  if(getFileNode!=-1)
+  {
+    writeFile(fileToWrite,getFileNode);
+  }
+  else
+  {
+    printf("\nSorry, could not find specified file in the file system!\n");
+  }
+}
+
+
+
 int main() {
   dir=(struct Directory_Entry*)&blocks[0];
   inodeList =(struct Inode*)&blocks[6];
   freeInodeList=(uint8_t*)&blocks[4];
   freeBlockList=(uint8_t*)&blocks[5];
 
-  createfs("disk.image");
+  //createfs("disk.image");
   open("disk.image");
   
-  dir[0].valid=1;
-  dir[1].valid=1;
-  dir[2].valid=1;
   int i;
-  for(i=0;i<128;i++) {
-    printf("%d: %d\n",i,dir[i].valid);
+  for(i=0;i<128;i++)
+  {
+    if(dir[i].valid==1)
+    {
+      printf("\n%d %d\n",dir[i].inode,inodeList[dir[i].inode].size);
+    }
   }
+
+  //put("secret2");
+  //get("secret","noSoSecret");
 
   close("disk.image");
   return 0;
